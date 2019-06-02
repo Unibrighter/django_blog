884f3becb5922494c4d707c25ac2c06388dc3b40
Kunliang Wu <kunliang@blitzm.com>
2019-06-02 18:23:38 +1000

    #1 format views in comment module, better urls and .gitignore

--- django_blog/comments/views.py
+++ django_blog/comments/views.py
@@ -14,28 +14,14 @@ def post_comment(request, post_id):
 
         if form.is_valid():
             comment = form.save(commit=False)
-
             comment.post = post
-
             comment.save()
-
             # return redirect(post)
-
         else:
             comment_list = post.comment_set.all()
-
             context = {'post': post,
-
                        'form': form,
-
                        'comment_list': comment_list
-
                        }
-
             return render(request, 'blog/single.html', context=context)
-
     return redirect(post)
-
-
-def test(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
