d39723e7796b6bc538a6c8d8216fb1509c007546
Kunliang Wu <kunliang@blitzm.com>
2019-06-02 18:35:03 +1000

    #1 install django-admin-material package to enable better admin theme

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_admin_material',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Australia/Melbourne'
 
 USE_I18N = True
 
