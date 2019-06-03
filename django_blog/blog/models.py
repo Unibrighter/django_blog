1ed0c40c34e22d508132c6cde4bb39be80946262
Kunliang Wu <kunliang@blitzm.com>
2019-06-03 15:14:16 +1000

    #3 Fixing the excerpt

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+from django.utils.html import strip_tags
+import markdown
 
 # Create your models here.
 class Category(models.Model):
@@ -29,6 +31,8 @@ class Post(models.Model):
 
     view_count = models.PositiveIntegerField(default = 0)
 
+    excerpt = models.CharField(max_length = 200, blank = True)
+
     def __str__(self):
         return self.title
 
@@ -38,6 +42,15 @@ class Post(models.Model):
     def increase_view_count(self):
         self.view_count += 1
         self.save(update_fields=['view_count'])
+
+    def save(self, *args, **kwargs):   
+        if (not self.excerpt):
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+        super(Post, self).save(*args, **kwargs)
 
     class Meta:
         ordering = ['-created_time','title']
