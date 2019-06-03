11acd4205ec5fb3735a76ef20961d6ebee04be13
Kunliang Wu <kunliang@blitzm.com>
2019-06-03 16:46:42 +1000

    #3 replace the custom views with class-based-view

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,58 +1,67 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
 from comments.forms import CommentForm
+
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 import markdown
 
 
-# Create your views here.
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
 
-def index(request):
-    post_list = Post.objects.all()
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['title'] = 'My new blog'
+        context['welcome'] = 'Welcome to my new blog!'
+        return context
 
-    return render(request=request,
-                  template_name='blog/index.html',
-                  context={
-                      'title': 'My new blog',
-                      'welcome': 'Welcome to my new blog!',
-                      'post_list': post_list
-                  })
+class ArchiveView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
 
-
-def archive(request, year, month):
-    post_list = Post.objects.filter(
-        created_time__year=year,
-        created_time__month=month
-    )
-
-    return render(request=request,
-                  template_name='blog/index.html',
-                  context={
-                      'post_list': post_list
-                  })
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super(ArchiveView, self).get_queryset().filter(created_time__year=year, created_time__month=month)
 
 
-def category(request, category_id):
-    category = get_object_or_404(Category, pk=category_id)
-    post_list = Post.objects.filter(category=category)
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/single.html'
+    context_object_name = 'post'
 
+    def get(self, request, *args, **kwargs):
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
 
-def detail(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
-    post.increase_view_count()
-    post.body = markdown.markdown(post.body,
-                                  extensions=[
-                                      'markdown.extensions.extra',
-                                      'markdown.extensions.codehilite',
-                                      'markdown.extensions.toc',
-                                  ])
+        self.object.increase_view_count()
+        return response
 
-    form = CommentForm()
-    comment_list = post.comment_set.all()
+    def get_object(self, queryset = None):
+        post = super(PostDetailView, self).get_object(queryset = None)
+        post.body = markdown.markdown(post.body,
+                                extensions=[
+                                    'markdown.extensions.extra',
+                                    'markdown.extensions.codehilite',
+                                    'markdown.extensions.toc',
+                                ])
+        return post
+    
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        context['form'] = CommentForm()
+        context['comment_list'] = self.object.comment_set.all()
+        return context
 
-    context = {
-        'post': post,
-        'form': form,
-        'comment_list': comment_list
-    }
-    return render(request=request, template_name='blog/single.html', context=context)
+class CategoryView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        category = get_object_or_404(Category, pk = self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category = category)
